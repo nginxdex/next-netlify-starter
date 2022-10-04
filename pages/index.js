1102d78837f5c79e3c770ea2cac02b5e5f924c91
nginxdex <34412504+nginxdex@users.noreply.github.com>
2022-10-04 15:58:20 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
+    <p>Semakin berkembangnya teknologi, semakin banyak pula cara untuk memanfaatkan kemajuan teknologi untuk melakukan kejahatan. Beberapa orang melakukan penipuan, tidak sedikit orang yang meretas komputer atau sistem dan merugikan orang lain untuk tujuan yang melanggar hukum.</p>
+<p>Sebagai orang biasa yang tak paham tentang IT, kita sering penasaran dengan keseharian para hacker. Nah, para pembuat film sebenarnya ingin menunjukkan betapa keren dan cerdasnya peretasan yang dilakukan hacker, karena tidak semua peretas selalu negatif. Berikut Daftar <a href="https://www.magelang1337.com/" target="_blank" rel="noopener"><strong>Film Hacker</strong></a> yang masih layak di tonton di tahun 2020 sampai 2023.</p>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
